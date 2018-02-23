c5a6a76f9793caff03089d5fdc817227987e6407
takuya-shibaura-it <cy15111@shibaura-it.ac.jp>
2018-02-23 12:28:04 +0900

    [fix]エラーの有無判定を修正

--- src/main/java/jp/co/tis/tiscon3/form/CardOrderForm.java
+++ src/main/java/jp/co/tis/tiscon3/form/CardOrderForm.java
@@ -34,22 +34,45 @@ public class CardOrderForm extends FormBase {
     private String gender;
 
     @NotBlank
-    @Size(max = 8)
-    @Pattern(regexp = "^[0-9]{3}-[0-9]{4}$")
-    private String zipCode;
+    @Size(max = 3)
+    @Pattern(regexp = "^[0-9]{3}$")
+    private String zipCode0;
+
+    @NotBlank
+    @Size(max = 4)
+    @Pattern(regexp = "^[0-9]{4}$")
+    private String zipCode1;
 
     @NotBlank
     @Size(max = 255)
     private String address;
 
     @NotBlank
-    @Size(max = 13)
-    @Pattern(regexp = "^0[0-9]{1,3}-[0-9]{2,4}-[0-9]{4}$")
-    private String homePhoneNumber;
+    @Size(max = 4)
+    @Pattern(regexp = "^0[0-9]{1,3}$")
+    private String homePhoneNumber0;
 
-    @Size(max = 13)
-    @Pattern(regexp = "^((070|080|090)-[0-9]{4}-[0-9]{4})?$")
-    private String mobilePhoneNumber;
+    @NotBlank
+    @Size(max = 4)
+    @Pattern(regexp = "^[0-9]{2,4}$")
+    private String homePhoneNumber1;
+
+    @NotBlank
+    @Size(max = 4)
+    @Pattern(regexp = "^[0-9]{4}$")
+    private String homePhoneNumber2;
+
+    @Size(max = 4)
+    @Pattern(regexp = "^(070|080|090)?$")
+    private String mobilePhoneNumber0;
+
+    @Size(max = 4)
+    @Pattern(regexp = "^([0-9]{4})?$")
+    private String mobilePhoneNumber1;
+
+    @Size(max = 4)
+    @Pattern(regexp = "^([0-9]{4})?$")
+    private String mobilePhoneNumber2;
 
     @NotBlank
     @Size(max = 255)
@@ -60,7 +83,7 @@ public class CardOrderForm extends FormBase {
     @Size(max = 20)
     private String spouse;
 
-    @NotBlank
+    //@NotBlank
     @Size(max = 20)
     private String houseClassification;
 
@@ -69,7 +92,7 @@ public class CardOrderForm extends FormBase {
     @Pattern(regexp = "[0-9]*")
     private String debt;
 
-    @NotBlank
+    //@NotBlank
     @Size(max = 120)
     private String job;
 
@@ -81,16 +104,28 @@ public class CardOrderForm extends FormBase {
     @Size(max = 255)
     private String employerName;
 
-    @Size(max = 8)
-    @Pattern(regexp = "^([0-9]{3}-[0-9]{4})?$")
-    private String employerZipCode;
+    @Size(max = 3)
+    @Pattern(regexp = "^([0-9]{3})?$")
+    private String employerZipCode0;
+
+    @Size(max = 4)
+    @Pattern(regexp = "^([0-9]{4})?$")
+    private String employerZipCode1;
 
     @Size(max = 255)
     private String employerAddress;
 
-    @Size(max = 13)
-    @Pattern(regexp = "^(0[0-9]{1,3}-[0-9]{2,4}-[0-9]{4})?$")
-    private String employerPhoneNumber;
+    @Size(max = 4)
+    @Pattern(regexp = "^(0[0-9]{1,3})?$")
+    private String employerPhoneNumber0;
+
+    @Size(max = 4)
+    @Pattern(regexp = "^([0-9]{2,4})?$")
+    private String employerPhoneNumber1;
+
+    @Size(max = 4)
+    @Pattern(regexp = "^([0-9]{4})?$")
+    private String employerPhoneNumber2;
 
     @Size(max = 255)
     private String industryType;
@@ -127,14 +162,12 @@ public class CardOrderForm extends FormBase {
         userFormName.put("zipCode0", "郵便番号（上3桁）");
         userFormName.put("zipCode1", "郵便番号（下4桁）");
         userFormName.put("address", "ご住所");
-        userFormName.put("homePhoneNumber", "自宅電話番号");
-        userFormName.put("homePhoneNumber0", "自宅電話番号");
-        userFormName.put("homePhoneNumber1", "自宅電話番号");
-        userFormName.put("homePhoneNumber2", "自宅電話番号");
-        userFormName.put("mobilePhoneNumber", "携帯電話番号");
-        userFormName.put("mobilePhoneNumber0", "携帯電話番号");
-        userFormName.put("mobilePhoneNumber1", "携帯電話番号");
-        userFormName.put("mobilePhoneNumber2", "携帯電話番号");
+        userFormName.put("homePhoneNumber0", "自宅電話番号（上2-4桁）");
+        userFormName.put("homePhoneNumber1", "自宅電話番号（中2-4桁）");
+        userFormName.put("homePhoneNumber2", "自宅電話番号（下4桁）");
+        userFormName.put("mobilePhoneNumber0", "携帯電話番号（上2-4桁）");
+        userFormName.put("mobilePhoneNumber1", "携帯電話番号（中2-4桁）");
+        userFormName.put("mobilePhoneNumber2", "携帯電話番号（下4桁）");
         userFormName.put("emailAddress", "メールアドレス");
         userFormName.put("spouse", "配偶者有無");
         userFormName.put("houseClassification", "お住まい");
@@ -143,14 +176,12 @@ public class CardOrderForm extends FormBase {
         userFormName.put("job", "ご職業");
 
         jobFormName.put("employerName", "お勤め先名");
-        jobFormName.put("employerZipCode", "お勤め先郵便番号");
         jobFormName.put("employerZipCode0", "お勤め先郵便番号（上3桁）");
         jobFormName.put("employerZipCode1", "お勤め先郵便番号（下4桁）");
         jobFormName.put("employerAddress", "お勤め先住所");
-        jobFormName.put("employerPhoneNumber", "お勤め先電話番号");
-        jobFormName.put("employerPhoneNumber0", "お勤め先電話番号");
-        jobFormName.put("employerPhoneNumber1", "お勤め先電話番号");
-        jobFormName.put("employerPhoneNumber2", "お勤め先電話番号");
+        jobFormName.put("employerPhoneNumber0", "お勤め先電話番号（上2-4桁）");
+        jobFormName.put("employerPhoneNumber1", "お勤め先電話番号（中2-4桁）");
+        jobFormName.put("employerPhoneNumber2", "お勤め先電話番号（下4桁）");
         jobFormName.put("industryType", "業種");
         jobFormName.put("capital", "資本金");
         jobFormName.put("companySize", "お勤め先の規模（従業員数）");
